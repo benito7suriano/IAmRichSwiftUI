aaf8f6b280a7a5a42dabbafccd1b600c4c7b606d
Beno Suriano <bjsuriano@gmail.com>
2021-11-02 14:28:42 -0400

    Creates I Am Rich app!.

--- I Am Rich SwiftUI/ContentView.swift
+++ I Am Rich SwiftUI/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.systemBlue)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I am Rich!")
+                    .font(.system(size: 40))
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone 8 Plus"))
     }
 }
